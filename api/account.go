b95fa0f5804121534ae9b6e7567cee1a94ae259f
Geun-Oh <kandy1002@naver.com>
2024-07-17 01:34:19 +0900

    Feat: add getAccount

--- api/account.go
+++ api/account.go
@@ -3,12 +3,18 @@ package api
 import (
 	db "github.com/Geun-Oh/backend/db/sqlc"
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 	"net/http"
+	"strconv"
 )
 
 type createAccountRequest struct {
 	Owner    string `json:"owner" binding:"required"`
 	Currency string `json:"currency" binding:"required"`
+}
+
+type getAccountRequest struct {
+	Id string `json:"id" binding:"required"`
 }
 
 func (s *Server) createAccount(ctx *gin.Context) {
@@ -26,6 +32,29 @@ func (s *Server) createAccount(ctx *gin.Context) {
 	}
 
 	account, err := s.store.CreateAccount(ctx, arg)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+func (s *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	parsedId, err := strconv.ParseInt(req.Id, 10, 64)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	account, err := s.store.GetAccount(ctx, parsedId)
 
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
